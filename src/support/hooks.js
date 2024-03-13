091d35e00e27369e1d29c410b07f83c8e3d658f0
Jerez Leonardo <73914243+Khimea@users.noreply.github.com>
2024-03-13 10:31:58 -0300

    Update hooks.js

--- src/support/hooks.js
+++ src/support/hooks.js
@@ -2,9 +2,11 @@
 const utils = require("./utils");
 const { Builder, Browser, Capabilities } = require("selenium-webdriver");
 const { Before, After } = require("@cucumber/cucumber");
-
+const options = new chrome.Options();
+options.addArguments('--headless');
 Before(async (scenario) => {
-  global.driver = new Builder().forBrowser(Browser.CHROME).build();
+  global.driver = new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build();
+  
   driver.manage().window().maximize();
 });
 
